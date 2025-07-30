b6f3c93a933d75ec29d4aa9998b4e2178c4fa79e
pshycodr <115141578+pshycodr@users.noreply.github.com>
2025-07-30 20:56:40 +0530

    update: cors position

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,13 +5,13 @@ import adminRouter from './routes/admin.routes'
 import clientRouter from './routes/clients.routes'
 
 const app = new Hono()
+app.use("/api/*", cors())
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
 
-app.use("/api/*", cors())
 
 app.route("/api/v1/client", clientRouter)
 app.route("/api/v1/admin", adminRouter)
