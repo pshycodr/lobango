908475b49756f02a52348c11113a1aa843f59df2
pshycodr <115141578+pshycodr@users.noreply.github.com>
2025-08-04 23:33:23 +0530

    add: PUBLIC_API_BASE_URL env

--- admin/src/lib/axios.ts
+++ admin/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8787",
+  baseURL: process.env.PUBLIC_API_BASE_URL || "http://localhost:8787",
   withCredentials: true
 });
 
