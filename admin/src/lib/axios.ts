3e5254353b0ec35822b137390daa6bed8f7ad61c
pshycodr <115141578+pshycodr@users.noreply.github.com>
2025-08-04 23:47:07 +0530

    fix: axios settings

--- admin/src/lib/axios.ts
+++ admin/src/lib/axios.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.PUBLIC_API_BASE_URL || "http://localhost:8787",
-  withCredentials: true
+  baseURL: import.meta.env.PUBLIC_API_BASE_URL || "http://localhost:8787",
+  withCredentials: true,
 });
 
 export default api;
