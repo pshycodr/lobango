b00d1eab8c5c3bf3d6d077eab6d64f5679dd284a
pshycodr <115141578+pshycodr@users.noreply.github.com>
2025-08-09 15:51:12 +0530

    add: navigation panel for mobile screens

--- admin/src/routes/__root.tsx
+++ admin/src/routes/__root.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { Outlet, createRootRoute } from '@tanstack/react-router'
+import Navigation from '../components/Common/Navigation'
 
 export const Route = createRootRoute({
   component: RootComponent,
@@ -10,6 +11,7 @@ function RootComponent() {
     <React.Fragment>
       {/* <div>Hello "__root"!</div> */}
       <Outlet />
+      <Navigation />
     </React.Fragment>
   )
 }
