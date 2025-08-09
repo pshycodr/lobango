2d0b8603455bb74feabe5a6493fb3d2239cc1285
pshycodr <115141578+pshycodr@users.noreply.github.com>
2025-08-09 18:20:40 +0530

    add: status bar for mobile devices

--- admin/src/routes/__root.tsx
+++ admin/src/routes/__root.tsx
@@ -1,15 +1,22 @@
 import * as React from 'react'
 import { Outlet, createRootRoute } from '@tanstack/react-router'
 import Navigation from '../components/Common/Navigation'
+import { StatusBar, Style } from '@capacitor/status-bar'
 
 export const Route = createRootRoute({
   component: RootComponent,
 })
 
 function RootComponent() {
+  React.useEffect(() => {
+    if (typeof window !== 'undefined' && (window as any).Capacitor) {
+      StatusBar.setStyle({ style: Style.Dark })
+      StatusBar.setBackgroundColor({ color: '#000000' })
+    }
+  }, [])
+
   return (
     <React.Fragment>
-      {/* <div>Hello "__root"!</div> */}
       <Outlet />
       <Navigation />
     </React.Fragment>
