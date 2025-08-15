235a7f0280aa7372355e8b71d03c50d90e227ed4
pshycodr <115141578+pshycodr@users.noreply.github.com>
2025-08-15 12:22:04 +0530

    fix: routing and text selecting on mobile

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,17 +1,38 @@
-import { createRouter, RouterProvider } from "@tanstack/react-router"
-import { routeTree } from "./routeTree.gen"
+import { App as CapacitorApp } from "@capacitor/app";
+import type { PluginListenerHandle } from "@capacitor/core";
+import { SplashScreen } from '@capacitor/splash-screen';
+import { useEffect } from "react";
 
 function App() {
 
-  const router = createRouter({ routeTree })
+  SplashScreen.hide(); 
+  
+  document.body.style.backgroundImage = 'none';
+  document.body.style.background = 'var(--eerie-black-2)'; 
+  
+  useEffect(() => {
+    let listener: PluginListenerHandle;
+  
+    (async () => {
+      listener = await CapacitorApp.addListener("backButton", ({ canGoBack }) => {
+        if (canGoBack || window.history.length > 1) {
+          window.history.back();
+        } else {
+          CapacitorApp.exitApp();
+        }
+      });
+    })();
+  
+    return () => {
+      listener?.remove();
+    };
+  }, []);
 
-  // declare module "@tanstack"
-
-  return (
-    <>
-      <RouterProvider router={router} />
-    </>
-  )
+  document.addEventListener('copy', e => {
+    e.preventDefault();
+    e.clipboardData?.setData('text/plain', window.getSelection()?.toString() || '');
+  });
+  
 }
 
-export default App
+export default App;
