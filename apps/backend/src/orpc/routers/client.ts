cd68b1416ed95286f0aa65467ae29eeab237fa0c
Anish Roy <aroy262692@gmail.com>
2026-08-09 22:01:34 +0530

    feat(placeOrder): implement palce order procedure

--- apps/backend/src/orpc/routers/client.ts
+++ apps/backend/src/orpc/routers/client.ts
@@ -1,4 +1,5 @@
 import { getOrdersbById } from "@/procedures/client/orders/getOrdersById";
+import { placeOrder } from "@/procedures/client/orders/placeOrder";
 import { getNewBookingPermission } from "@/procedures/client/permisson/getNewBookingPermission";
 import { getNewOrderPermission } from "@/procedures/client/permisson/getNewOrderPermission";
 
@@ -9,6 +10,7 @@ export const clientRouter = {
   },
   order: {
     getOrdersbById: getOrdersbById,
+    placeOrder: placeOrder,
   },
 };
 
---
+++ apps/backend/src/utils/generateOrderId.ts
@@ -0,0 +1,6 @@
+import { customAlphabet } from "nanoid";
+
+export const generateOrderId = () => {
+  const nanoid = customAlphabet("0123456789abcdefghijklmnopqrstuvwxyz", 10);
+  return `ORD_${nanoid()}`;
+};
