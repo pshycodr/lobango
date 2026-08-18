c458bdf4a59a0b2ce94717d902aee8030976aebf
Anish Roy <aroy262692@gmail.com>
2026-08-18 15:15:19 +0530

    refactor(frontend)): update axios import and enhance utility function typings

--- apps/frontend/lib/orpc.ts
+++ apps/frontend/lib/orpc.ts
@@ -1,11 +1,10 @@
 import type { ClientRouter } from "@lobango/backend/rpc/client";
 import { createORPCClient } from "@orpc/client";
 import { RPCLink } from "@orpc/client/fetch";
-import { RouterClient } from "@orpc/server";
+import type { RouterClient } from "@orpc/server";
 
-const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
-
-console.log("BASE_URL: ", BASE_URL);
+const BASE_URL =
+  process.env.NEXT_PUBLIC_API_BASE_URL || "http://127.0.0.1:8787";
 
 const link = new RPCLink({
   url: `${BASE_URL}/api/v1/client`,
