2be0d23248c219f1694fc5084c4ef964891af55b
Anish Roy <aroy262692@gmail.com>
2026-08-20 16:59:35 +0530

    feat: refactor nav-bar and update all the page.tsx

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -1,17 +1,17 @@
-import AboutSection from "@/components/HomePage/AboutSection";
-import BackToTop from "@/components/HomePage/BackToTop";
-import FeaturesSection from "@/components/HomePage/FeaturesSection";
-import FooterSection from "@/components/HomePage/FooterSection";
-import Header from "@/components/HomePage/Header";
-import HeroSection from "@/components/HomePage/Hero";
-import HomeMenuSection from "@/components/HomePage/HomeMenuSection";
-import Preloader from "@/components/HomePage/PreLoader";
-import ReservationSection from "@/components/HomePage/ReservationTable";
-import ServiceSection from "@/components/HomePage/ServiceSection";
-import TestimonialSection from "@/components/HomePage/TestimonialSection";
+import { AboutSection } from "@/components/HomePage/AboutSection";
+import { BackToTop } from "@/components/HomePage/BackToTop";
+import { FeaturesSection } from "@/components/HomePage/FeaturesSection";
+import { FooterSection } from "@/components/HomePage/FooterSection";
+import { Header } from "@/components/HomePage/Header";
+import { HeroSection } from "@/components/HomePage/Hero";
+import { HomeMenuSection } from "@/components/HomePage/HomeMenuSection";
+import { Preloader } from "@/components/HomePage/PreLoader";
+import { ReservationSection } from "@/components/HomePage/ReservationTable";
+import { ServiceSection } from "@/components/HomePage/ServiceSection";
+import { TestimonialSection } from "@/components/HomePage/TestimonialSection";
 import { DM_Sans, Forum } from "next/font/google";
+import React from "react";
 
-// Import Google Fonts
 const dmSans = DM_Sans({
   subsets: ["latin"],
   weight: ["400", "700"],
@@ -24,7 +24,7 @@ const forum = Forum({
   variable: "--font-forum",
 });
 
-export default function Home() {
+export default function HomePage() {
   return (
     <div className={`${dmSans.variable} ${forum.variable}`}>
       <Preloader />
