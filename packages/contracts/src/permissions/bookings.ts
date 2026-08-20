55bac365949189395e945875f06adc35712e8a80
Anish Roy <aroy262692@gmail.com>
2026-08-20 17:24:20 +0530

    feat(permissions): restructure permissions module by moving interfaces to dedicated files

---
+++ packages/contracts/src/permissions/bookings.ts
@@ -0,0 +1,11 @@
+export interface GetNewBookingPermissionResponse {
+  new_bookings: boolean;
+}
+
+export interface SetNewBookingPermissionRequest {
+  value: boolean;
+}
+
+export interface SetNewBookingPermissionResponse {
+  new_bookings: boolean;
+}
---
+++ packages/contracts/src/permissions/orders.ts
@@ -0,0 +1,11 @@
+export interface GetNewOrderPermissionResponse {
+  new_orders: boolean;
+}
+
+export interface SetNewOrderPermissionRequest {
+  value: boolean;
+}
+
+export interface SetNewOrderPermissionResponse {
+  new_orders: boolean;
+}
